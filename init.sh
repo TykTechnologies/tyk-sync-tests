016b8e9f055fdc5fc0580e401b82e666fd78f72b
Burak Sekili <buraksekili@gmail.com>
2024-06-07 16:45:16 +0300

    run new test suite
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- init.sh
+++ init.sh
@@ -2,9 +2,19 @@
 
 set -e
 
+echo "... Clean Tyk resources"
+TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./clean-apis.sh
+echo "... Created"
+
 echo "... Creating 5 Classic, 5 OAS API Definitions and 5 Policies"
 MAX=5 TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./create-apis.sh
 echo "... Created"
 
-venom run -vv ./tests/dump.yaml --var="TYK_AUTH=$TYK_AUTH"  --var="TYK_URL=$TYK_URL"
-venom run -vv ./tests/sync.yaml --var="TYK_AUTH=$TYK_AUTH"  --var="TYK_URL=$TYK_URL"
+echo "... Testing tyk-sync dump"
+venom run --output-dir=logs --stop-on-failure -vv ./tests/dump.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
+
+echo "... Testing tyk-sync sync"
+venom run --output-dir=logs --stop-on-failure -vv ./tests/sync.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
+
+echo "Running dump-2-apis.yaml"
+venom run --output-dir=logs --stop-on-failure -vv ./tests/dump-2-apis.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
