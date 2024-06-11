e9ed27ee7867723e1e9408f5e77ab6f1a1940e75
Burak Sekili <buraksekili@gmail.com>
2024-06-11 15:09:06 +0300

    restructure test suite flow
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- init.sh
+++ init.sh
@@ -2,31 +2,43 @@
 
 set -e
 
-echo "... Clean test folder"
-rm -f ./test/*.json
-rm -f ./test/.tyk.json
-sleep 1;
-echo "... Cleaned"
+initTykResources() {
+  echo "... Clean test folder"
+  rm -f ./test/*.json
+  rm -f ./test/.tyk.json
+  sleep 1;
+  echo "... Cleaned"
 
-echo "... Clean resources on Tyk"
-TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./clean-apis.sh
-echo "... Cleaned"
+  echo "... Clean resources on Tyk"
+  TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./clean-apis.sh
+  echo "... Cleaned"
 
-echo "... Creating 5 Classic, 5 OAS API Definitions and 5 Policies"
-MAX=5 TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./create-apis.sh
-echo "... Created"
+  echo "... Creating 5 Classic, 5 OAS API Definitions and 5 Policies"
+  TYK_AUTH=$TYK_AUTH TYK_URL=$TYK_URL ./create-apis.sh
+  echo "... Created"
+}
+
+initTykResources
 
 echo "... Testing tyk-sync dump"
 venom run --output-dir=logs --stop-on-failure -vv ./tests/dump.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
 
+initTykResources
+
 echo "... Testing tyk-sync sync"
 venom run --output-dir=logs --stop-on-failure -vv ./tests/sync.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
+
+initTykResources
 
 echo "... Testing tyk-sync dump command with specific APIs"
 venom run --output-dir=logs --stop-on-failure -vv ./tests/dump-specific-apis.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
 
+initTykResources
+
 echo "... Testing tyk-sync publish command to update resources"
 venom run --output-dir=logs --stop-on-failure -vv ./tests/publish.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
+
+initTykResources
 
 echo "... Testing tyk-sync update command to update resources"
 venom run --output-dir=logs --stop-on-failure -vv ./tests/update.yaml --var="TYK_AUTH=$TYK_AUTH" --var="TYK_URL=$TYK_URL"
